8ffd27d974fd5bdde4e32ed0b6c847b1f6a794f6
nickbastos <nickbastos4@gmail.com>
2020-11-03 12:09:36 -0600

    Large style changes, new static folder"

--- src/promotions-manager/promotions-manager-ui/src/Header.js
+++ src/promotions-manager/promotions-manager-ui/src/Header.js
@@ -3,40 +3,45 @@ import NavigationControl from "./Components/Navigation/NavigationControl";
 import LoginControl from "./Components/Auth/LoginControl";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import gourmetLogo from "./gourmetIncLogo.png";
 
-const headerLogoStyle = {
-  height: "30%",
-  width: "30%",
-};
 const headerStyle = {
   display: "flex",
   flexDirection: "row",
   alignItems: "center",
-  justifyContent: "center",
+  justifyContent: "flex-start",
+  width: "60%",
+  marginLeft: "-20%",
 };
 const promoManagerStyle = {
   fontSize: "20px",
+  marginTop: "-0.5%",
+  marginRight: "10%",
 };
+
+const navbarStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
 const Header = () => (
   <Navbar inverse collapseOnSelect staticTop={true}>
-    <Navbar.Header style={headerStyle}>
-      <Navbar.Brand>
-        <img src={gourmetLogo} style={headerLogoStyle} alt="Gourmet Inc Logo" />
-      </Navbar.Brand>
-      <Navbar.Brand>
-        <Link to="/" style={promoManagerStyle}>
-          Promo Manager
-        </Link>
-      </Navbar.Brand>
-      <NavigationControl />
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <LoginControl />
-      </Nav>
-    </Navbar.Collapse>
+    <div style={navbarStyle}>
+      <Navbar.Header style={headerStyle}>
+        <Navbar.Brand>
+          <Link to="/" style={promoManagerStyle}>
+            PROMO MANAGER
+          </Link>
+        </Navbar.Brand>
+        <NavigationControl />
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <LoginControl />
+        </Nav>
+      </Navbar.Collapse>
+    </div>
   </Navbar>
 );
 
--- src/promotions-manager/promotions-manager-ui/src/Main.js
+++ src/promotions-manager/promotions-manager-ui/src/Main.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import {Grid, Col, Row} from "react-bootstrap";
+import React from "react";
+import { Grid, Col, Row } from "react-bootstrap";
 import MainRouter from "./MainRouter";
 
-const Main  = () => (
-    <Grid fluid={true}>
-        <Row>
-            <Col md={8} mdOffset={2}>
-                <MainRouter />
-            </Col>
-        </Row>
-    </Grid>
-)
+const Main = () => <MainRouter />;
 
 export default Main;
+
+{
+  /* <Grid fluid={true}>
+<Row>
+    <Col md={8} mdOffset={2}>
+        <MainRouter />
+    </Col>
+</Row>
+</Grid> */
+}
