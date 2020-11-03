6bf9971c1a212010e8516d4e9b3cae898bd63422
nickbastos <nickbastos4@gmail.com>
2020-11-02 18:43:42 -0600

    Added logo to nav bar, centered signup form in home and changed sign up button style in home

--- src/promotions-manager/promotions-manager-ui/src/Header.js
+++ src/promotions-manager/promotions-manager-ui/src/Header.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React from "react";
 import NavigationControl from "./Components/Navigation/NavigationControl";
 import LoginControl from "./Components/Auth/LoginControl";
-import {Navbar, Nav} from "react-bootstrap";
-import {Link} from "react-router-dom";      
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import gourmetLogo from "./gourmetIncLogo.png";
 
-const Header = () => (   
-    <Navbar inverse collapseOnSelect staticTop={true}>
-        <Navbar.Header>   
-            <Navbar.Brand>                
-                <Link to='/'>Promo Manager</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-            <NavigationControl />
-            <Nav pullRight>
-                <LoginControl />
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-)
+const headerLogoStyle = {
+  height: "30%",
+  width: "30%",
+};
+const headerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+};
+const promoManagerStyle = {
+  fontSize: "20px",
+};
+const Header = () => (
+  <Navbar inverse collapseOnSelect staticTop={true}>
+    <Navbar.Header style={headerStyle}>
+      <Navbar.Brand>
+        <img src={gourmetLogo} style={headerLogoStyle} alt="Gourmet Inc Logo" />
+      </Navbar.Brand>
+      <Navbar.Brand>
+        <Link to="/" style={promoManagerStyle}>
+          Promo Manager
+        </Link>
+      </Navbar.Brand>
+      <NavigationControl />
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <LoginControl />
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 export default Header;
